e089e7a5a5355680f67c22254afc1c422ca33050
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2021-07-03 03:36:29 +0300

    Buton oluşturuldu

--- flutter_calisma/lib/main.dart
+++ flutter_calisma/lib/main.dart
@@ -7,19 +7,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Mobil Uygulamam",
-      home: AnaEkran(),
+      home: Iskele(),
     );
   }
 }
 
-class AnaEkran extends StatelessWidget {
+class Iskele extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Menü"),
       ),
-      body: Container(),
+      body: AnaEkran(),
+    );
+  }
+}
+class AnaEkran extends StatefulWidget {
+  @override
+  _AnaEkranState createState() => _AnaEkranState();
+}
+
+class _AnaEkranState extends State<AnaEkran> {
+
+   String blogYazisi='Dert Dinlenir :)';
+
+   dertDinle(){
+     setState(() {
+        blogYazisi='Dinleniyor';
+     });
+   }
+   tamamSus(){
+     setState(() {
+        blogYazisi='Susturuldu';
+     });
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center ,
+          children: <Widget>[
+            Text(blogYazisi),
+            ElevatedButton(onPressed:dertDinle , child:Text('Dinlemeye Başla')),
+            ElevatedButton(onPressed:tamamSus , child:Text('Sus')),
+           
+          ],)
+      ),
     );
   }
 }
