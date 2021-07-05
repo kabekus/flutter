3efe9721bc6369d199e6125185f74e6a287920e5
Kabe KUŞ <74798718+kabekus@users.noreply.github.com>
2021-07-05 17:08:53 +0300

    Sayfalar arası geçiş işlemleri uygulandı

--- flutter_calisma/lib/main.dart
+++ flutter_calisma/lib/main.dart
@@ -6,105 +6,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mesajlaşma Uygulaması",
-      home: Iskele(),
+      initialRoute:"/",
+      routes: {
+        "/": (context)=> GirisEkrani(),
+        "/ProfilSayfasi": (context)=> ProfilSayfasi(),
+      },
     );
   }
 }
-
-class Iskele extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Konuş"),
-      ),
-      body: AnaEkran(),
-    );
-  }
-}
-
-class AnaEkran extends StatefulWidget {
-  @override
-  _AnaEkranState createState() => _AnaEkranState();
-}
-
-class _AnaEkranState extends State<AnaEkran> {
-  TextEditingController t1 = TextEditingController();
-
-  List<MesajBalonu> mesajListesi = [];
-
-  listeyeEkle(String gelenMesaj) {
-    setState(() {
-      MesajBalonu mesajNesnesi = MesajBalonu(mesaj: gelenMesaj);
-      mesajListesi.insert(0, mesajNesnesi);
-      t1.clear();
-    });
+ class GirisEkrani extends StatefulWidget {
+   @override
+   _GirisEkraniState createState() => _GirisEkraniState();
+ }
+ 
+ class _GirisEkraniState extends State<GirisEkrani> {
+  
+  girisButon(){
+    Navigator.pushNamed(context, "/ProfilSayfasi");
   }
 
-  Widget sohbetKutusu() {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Flexible(
-            child: TextField(
-              onSubmitted:listeyeEkle,
-              controller: t1,
-            ),
-          ),
-          IconButton(
-            onPressed: () => listeyeEkle(t1.text),
-            icon: Icon(Icons.send_outlined),
-          ),
-        ],
-      ),
-    );
-  }
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(title: Text("Giriş")),
+       body: Container(
+         child: Column(
+           children: <Widget> [
+             ElevatedButton(onPressed: girisButon, child:Text("Giriş Yap"))
+           ],
+         ),
+         ),
+     );
+   }
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              reverse: true,
-                itemCount: mesajListesi.length,
-                itemBuilder: (_, indeksNo) => mesajListesi[indeksNo]),
-          ),
-           Divider(thickness:3,),
-          sohbetKutusu(),
-        ],
-      ),
-    );
-  }
-}
+ class ProfilSayfasi extends StatefulWidget {
+   @override
+   _ProfilSayfasiState createState() => _ProfilSayfasiState();
+ }
+ 
+ class _ProfilSayfasiState extends State<ProfilSayfasi> {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(title: Text("Profilim"),),
+     );
+   }
+ }
 
-String isim = "User";
-String mesaj;
-
-class MesajBalonu extends StatelessWidget {
-  String mesaj;
-
-  MesajBalonu({this.mesaj});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10 ),
-      child: Row(
-        children: <Widget>[
-          CircleAvatar(
-            child: Text(isim [0]),),
-          Column(
-            children: <Widget>[
-              Text(""),
-              Text(isim),
-              Text(mesaj + '\n'),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
